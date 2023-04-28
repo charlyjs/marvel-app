4c8819b8ba8aa58d2c89f948e06fdf4b5d02de8a
Juan Carlos <mdz.jcarlos@gmail.com>
2023-04-28 16:22:21 -0600

    feat: add config tanstack react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 
@@ -8,8 +9,12 @@ import './i18n';
 // Components
 import RouteApp from './components/RouteApp.tsx';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouteApp />
+    <QueryClientProvider client={queryClient}>
+      <RouteApp />
+    </QueryClientProvider>
   </React.StrictMode>
 );
