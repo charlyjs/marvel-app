815edc04414f95ddc8a72de20859b65d53263335
Juan Carlos <mdz.jcarlos@gmail.com>
2023-04-27 23:04:51 -0600

    feat: add style css for page

--- src/pages/ErrorPageNoFound.tsx
+++ src/pages/ErrorPageNoFound.tsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
+import deadpool from '../assets/deadpool-404.png';
+
 export default function ErrorPageNoFound() {
   return (
-    <h1 className='text-white'>ErrorPageNoFound</h1>
+    <div className='text-center text-white'>
+      <h1 className='text-7xl text-rose-700'>Página no encontrada</h1>
+
+      <div className='mt-10'>
+        <img src={deadpool} alt='deadpool-404' className='mx-auto' />
+        <p className='text-3xl'>
+          Lo sentimos, la página que estás buscando no existe.
+        </p>
+      </div>
+
+      <div className='mt-8'>
+        <Link to='/' className='mx-auto rounded-lg bg-amber-600 pt-1 pb-1 w-36 block'>
+          Regresar
+        </Link>
+      </div>
+    </div>
   );
 }
